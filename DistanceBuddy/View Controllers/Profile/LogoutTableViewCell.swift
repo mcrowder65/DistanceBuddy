1ef0ac29fc7e3279334b44c172b29f3b466957d1
Matt Crowder <mcrowder65@gmail.com>
2019-12-13 07:21:11 -0500

    trying to add some tableview stuff

---
+++ DistanceBuddy/View Controllers/Profile/LogoutTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  LogoutTableViewCell.swift
+//  DistanceBuddy
+//
+//  Created by Matt Crowder on 12/13/19.
+//  Copyright © 2019 Matt. All rights reserved.
+//
+
+import UIKit
+
+class LogoutTableViewCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
